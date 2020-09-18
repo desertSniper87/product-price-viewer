058e5e8c0d9a8393366b864021d92494240eafa4
desertsniper87 <torshobuet@gmail.com>
2020-09-18 16:08:59 +0600

    initial commit

---
+++ src/routes/Prices.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from 'react-router-dom';
+
+const Prices = () => {
+    return (
+        <div>
+            <h1> Prices </h1>
+
+            <Link to="/prices">Prices</Link>
+        </div>
+    );
+}
+
+export default Prices;
