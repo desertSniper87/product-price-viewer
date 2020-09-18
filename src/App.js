3d8c26cfba2c51586a975af6cf3f92d900347514
desertsniper87 <torshobuet@gmail.com>
2020-09-18 16:17:06 +0600

    router basic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from './routes/Home';
+import Prices from './routes/Prices';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <route path="/">
+          <route path="/" exact>
             <Home />
           </route>
           <Route path="/prices">
-            
+            <Prices />
+
           </Route>
         </Switch>
       </BrowserRouter> 
